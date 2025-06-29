50c8caef7b8e3ca21dfe21816c17b9bbe73106f6
mmmmm26-ux <av4281219@gmail.com>
2025-06-29 12:11:17 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const indexRoutes = require('./routes/index');
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use('/', indexRoutes);
+
+app.listen(port, () => {
+  console.log(`Servidor en http://localhost:${port}`);
+});
