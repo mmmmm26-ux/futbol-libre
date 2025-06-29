59535d3cdaaa3943575fa0e34dd0e1b253e922c2
mmmmm26-ux <av4281219@gmail.com>
2025-06-29 12:34:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
+
 const indexRoutes = require('./routes/index');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 app.use('/', indexRoutes);
 
 app.listen(port, () => {
