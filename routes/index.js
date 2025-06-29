16a48ad6b18a1e79cacc16058e401832b1e3b5bd
mmmmm26-ux <av4281219@gmail.com>
2025-06-29 12:35:50 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// ⚠ Enlaces FUNCIONALES estilo Fútbol Libre (actualizados)
 const partidos = [
   {
     equipoA: "Colombia",
